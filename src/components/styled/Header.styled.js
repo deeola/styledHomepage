bf1cbb36391bd523bec714ffb9d205256cc5cebb
Adeola Bamigboye <clazikar@gmail.com>
2021-10-08 18:09:43 +0200

    Add Nav list and styled

--- src/components/styled/Header.styled.js
+++ src/components/styled/Header.styled.js
@@ -1,17 +1,40 @@
 import styled from "styled-components";
 
-export const HeaderStyled = styled.header``;
-
-export const NavStyled = styled.nav`
-  &:h1 {
-    color: white;
-  }
+export const HeaderStyled = styled.header`
+  display: flex;
+  height: 100vh;
 `;
 
-export const HeaderLeft = styled.div``;
+export const HeaderLeft = styled.div`
+  background-color: red;
+  width: 50%;
+`;
 
-export const HeaderRight = styled.div``;
+export const HeaderRight = styled.div`
+  width: 50%;
+  background-color: green;
+`;
 
-export const Logo = styled.p``;
+export const NavStyled = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 50%;
+  margin: 20px 0px 20px 20px;
+`;
 
-export const UnorderedList = styled.ul``;
+export const Logo = styled.p`
+  color: white;
+`;
+
+export const UnorderedList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
+  li {
+    list-style: none;
+    p {
+      color: green;
+    }
+  }
+`;
